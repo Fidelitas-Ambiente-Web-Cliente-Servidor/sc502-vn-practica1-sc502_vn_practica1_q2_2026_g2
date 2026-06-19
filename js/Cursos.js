08bc9d581d684e4182a84ddbb97286c83b3dbb5a
DerekVega <dereckvb9@gmail.com>
2026-06-18 18:25:16 -0600

    feat: add Cursos.js file with course data

---
+++ js/Cursos.js
@@ -0,0 +1,50 @@
+const cursos = [
+    {
+        nombre: "HTML5",
+        descripcion: "Creación de la estructura de sitios web modernos.",
+        categoria: "Desarrollo Web",
+        duracion: "20 horas",
+        precio: "$50",
+        imagen: "./Imagenes/html.png"
+    },
+    {
+        nombre: "CSS3",
+        descripcion: "Diseño y personalización visual de páginas web.",
+        categoria: "Desarrollo Web",
+        duracion: "25 horas",
+        precio: "$60",
+        imagen: "./Imagenes/css.png"
+    },
+    {
+        nombre: "JavaScript",
+        descripcion: "Programación interactiva para aplicaciones web.",
+        categoria: "Desarrollo Web",
+        duracion: "30 horas",
+        precio: "$80",
+        imagen: "./Imagenes/javascript.png"
+    },
+    {
+        nombre: "Java",
+        descripcion: "Desarrollo de aplicaciones orientadas a objetos.",
+        categoria: "Programación",
+        duracion: "40 horas",
+        precio: "$120",
+        imagen: "./Imagenes/java.png"
+    },
+    {
+        nombre: "Python",
+        descripcion: "Automatización y desarrollo de software moderno.",
+        categoria: "Programación",
+        duracion: "35 horas",
+        precio: "$100",
+        imagen: "./Imagenes/python.jpeg"
+    },
+    {
+        nombre: "MySQL",
+        descripcion: "Gestión y administración de bases de datos.",
+        categoria: "Base de Datos",
+        duracion: "20 horas",
+        precio: "$70",
+        imagen: "./Imagenes/mysql.png"
+    }
+];
